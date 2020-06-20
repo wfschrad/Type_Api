579ad613033bc650c007eed71e15722bc3c1b9f8
William Schrader <wfschrad@gmail.com>
2020-06-20 15:23:54 -0700

    Polish personality typing questions.

--- seeders/01-add-typing-questions.js
+++ seeders/01-add-typing-questions.js
@@ -5,336 +5,336 @@ module.exports = {
     return queryInterface.bulkInsert('TypingQuestions', [
       {
         typeAttributeId: 1,
-        content: 'tend to talk first, think second.',
+        content: 'I tend to talk first, and think second.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 1,
-        content: "know a lot of people, and see many of them as close friends",
+        content: "I know a lot of people, and see many of them as close friends",
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 1,
-        content: 'enjoy background noise, such as the tv or radio, while reading or having a conversation.',
+        content: 'I enjoy background noise, such as the tv or radio, while reading or having a conversation.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 1,
-        content: 'find phone calls welcome interruptions.',
+        content: "I find phone calls welcome interruptions from the day's tasks.",
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 1,
-        content: 'find listening more difficult than speaking.',
+        content: 'I find listening more difficult than speaking.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 1,
-        content: 'prefer generating ideas in a group vs individually.',
+        content: 'I prefer generating ideas in a group as opposed to individually.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 1,
-        content: 'prefer to spend special occasions with only a few close friends.',
+        content: 'I prefer to spend special occasions with only a few close friends.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 1,
-        content: 'allow others to state their opinions without interruption, and appreciate the same in return',
+        content: 'I allow others to state their opinions without interruption, and expect the same in return',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 1,
-        content: 'often rehearse things before saying them',
+        content: 'I often rehearse things before saying them',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 1,
-        content: 'enjoy recharging with some solo time after being in a group.',
+        content: 'I enjoy recharging with solo time after being in a group.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 1,
-        content: 'believe talk is cheap.',
+        content: 'I believe talk is cheap.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 1,
-        content: 'sometimes feel taken advantage of by being a better listener than others.',
+        content: 'I sometimes feel taken advantage of by being a better listener than others.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: "would rather 'do' something than 'think' about it.",
+        content: "I would rather 'do' something than 'think' about it.",
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: "like specific answers to questions, you prefer 'three fifty-two' vs 'almost 4.'",
+        content: "I prefer specific answers to questions. If I ask the time, I would rather the answer be 'three fifty-two' vs 'almost 4.'",
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: "believe if it ain't broke, don't fix it.",
+        content: "I believe in the motto 'if it ain\'t broke, don't fix it.'",
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: "get frustated when people don't give clear instructions.",
+        content: "I get frustated when people don't give clear instructions.",
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: 'prefer to receive information sequentially instead of in a random order.',
+        content: 'I prefer to receive information sequentially instead of in a random order.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: 'would rather work with facts and figures than ideas and theories.',
+        content: 'I would rather work with facts and figures than ideas and theories.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: 'are often accused of being absent-minded.',
+        content: 'I am often accused of being absent-minded.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: 'tend to think about lots of things at once.',
+        content: 'I tend to think about lots of things at once.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: 'find details boring.',
+        content: 'I find details boring.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: 'tend to give general answers to most questions, and get annoyed when pushed for specifics.',
+        content: 'I tend to give general answers to most questions, and get annoyed when pushed for specifics.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: "am usually more excited about where I'm going vs where I've been.",
+        content: "I am usually more excited about where I'm going vs where I've been.",
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 2,
-        content: 'am prone to puns and word games',
+        content: 'I am prone to puns and word games',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: 'often can stay cool and calm when others are upset.',
+        content: 'I can often stay cool and calm when others are upset.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: "believe disputes should be resolved based on what's fair instead of what will make everyone happy.",
+        content: "I believe disputes should be resolved based on what's fair instead of what will make everyone happy.",
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: 'would rather tell others when I disagree instead of letting them think they are right.',
+        content: 'I would rather tell others when I disagree instead of letting them think they are right.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: 'pride myself on my objectivity',
+        content: 'I pride myself on my objectivity',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: 'enjoy arguing both sides of an argument for the sake of intellectual exploration.',
+        content: 'I often enjoy arguing both sides of an argument for the sake of intellectual exploration.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: 'prefer being right over being liked',
+        content: 'I would rather be right than be liked',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: 'often asks how a decision will affect others.',
+        content: 'I often ask how a decision will affect others.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: "believe a 'good' decision takes other people's feelings into account.",
+        content: "I believe a 'good' decision takes other people's feelings into account.",
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: 'will often sacrifice your own happiness for benefit of others.',
+        content: 'I will often sacrifice my own happiness for the benefit of others.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: 'prefer harmony over clarity',
+        content: 'I prefer harmony over clarity',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: 'are frequently accused of taking things too seriously.',
+        content: 'I am frequently accused of taking things too seriously.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 3,
-        content: "won't hesistate to take something back if I perceive it has hurt somebody's feelings.",
+        content: "I won't hesistate to take something back if I perceive it has hurt somebody's feelings.",
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: 'am easily distracted; I can easily get lost before the front door and the car.',
+        content: 'I am easily distracted; I can easily get lost before the front door and the car.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: "love to explore the unknown.",
+        content: "I love to explore the unknown.",
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: 'hate planning and would rather just get started and see what the task demands.',
+        content: 'I hate planning and would rather just get started and see what the task demands.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: 'usually depend on last minute bursts of energy to meet deadlines',
+        content: 'I usually depend on last minute bursts of energy to meet deadlines',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: 'believe creativity and responsiveness trump order and neatness.',
+        content: 'I believe creativity and responsiveness trump order and neatness.',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: 'are often accused of being disorganized',
+        content: 'I am often accused of being disorganized',
         scoringScalar: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: "don't like surprises, and make this well known",
+        content: "I don't like surprises, and make this well known",
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: "keep lists and use them",
+        content: "I keep lists and actually use them",
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: 'thrive on order, often keeping systems for things like my refrigerator or closet.',
+        content: 'I thrive on order, often keeping systems for things like my refrigerator or closet.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: 'have a place for everything',
+        content: 'I have a place for everything',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: 'seem to always be waiting for others, who are almost never on time.',
+        content: 'I seem to always be waiting for others, who are almost never on time.',
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         typeAttributeId: 4,
-        content: "like to work things through to completion, even if I know I'll have to do it again to get it right.",
+        content: "I like to work things through to completion, even if I know I'll have to do it again to get it right.",
         scoringScalar: -1,
         createdAt: new Date(),
         updatedAt: new Date()
