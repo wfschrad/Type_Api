d4df25d40388ff8fee8e782c16e6dd45529c0749
William Schrader <wfschrad@gmail.com>
2020-06-22 02:53:29 -0700

    Update user object with path to s3-hosted profile photo.

--- routes/image-upload.js
+++ routes/image-upload.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const { User } = require('../models');
 
 const upload = require('../services/file-upload');
 const singleUpload = upload.single('image');
 
-
-router.post('/image-upload', (req, res) => {
-    singleUpload(req, res, (err) => {
+//use route structure to pass user id
+router.post('/image-upload/:id(\\d+)', (req, res) => {
+    singleUpload(req, res, async (err) => {
         if (err) {
             console.log('error uploading');
             console.log(err);
             return res.status(422).send({ errors: [{ title: 'File format not allowed', detail: err.message }] });
         } else {
-            console.log('req: ', req)
+
+            // post to db
+            console.log('params id: ', req.params.id);
+
+            const user = await User.findByPk(req.params.id, {
+                attributes: ['id']
+            });
+
+            if (user) await user.update({
+                profilePhoto: req.file.location
+            });
+            //what's best way to get user?
+
+
+            console.log('req inside: ', req.body)
+
             return res.json({ 'imageUrl': req.file.location });
         }
     });
+    console.log('req params outside: ', req.params.id)
+
 });
 
 module.exports = router;
